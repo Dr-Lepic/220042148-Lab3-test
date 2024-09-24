50b9846c0129b429944ab37169a1bba2f321675f
Dr-Lepic <mbmahbub007@gmail.com>
2024-09-24 12:46:26 +0600

    created NumWordPrinter class

---
+++ src/Test/TestLab3.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TestLab3 {
+
+    @test
+    public void even_1(){
+
+
+    }
+
+
+}
