8f270770fac325de08fd6717256a05a9c4b0b8ce
Dr-Lepic <mbmahbub007@gmail.com>
2024-09-25 20:25:27 +0600

    Tested the printWord method for EvenPrimeEleven, PrimeEleven

--- src/Test/TestLab3.java
+++ src/Test/TestLab3.java
@@ -24,5 +24,50 @@ public class TestLab3 {
         assertEquals("Even", printer.printWord(8));
     }
 
+    @Test
+    public void primeEleven_1(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("PrimeEleven", printer.printWord(55));
+    }
+
+    @Test
+    public void primeEleven_2(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("PrimeEleven", printer.printWord(33));
+    }
+
+    @Test
+    public void primeEleven_3U(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("PRIMEELEVEN", printer.printWordCap(77));
+    }
+
+    @Test
+    public void evenPrimeEleven_1U(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("EVENPRIMEELEVEN", printer.printWordCap(66));
+    }
+
+    @Test
+    public void evenPrimeEleven_2(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("EvenPrimeEleven", printer.printWord(22));
+    }
+
+    @Test
+    public void evenPrimeEleven_3(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("EvenPrimeEleven", printer.printWord(44));
+    }
+
+    @Test
+    public void evenPrimeLucky_1(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("evenPrimeLucky", printer.printWord(284));
+    }
+
+
+
+
 
 }
