4134bb7ccf7dc6197f58769929a1fd3aa0b08e29
Dr-Lepic <mbmahbub007@gmail.com>
2024-09-25 20:16:28 +0600

    Tested the printWord method for even numbers

--- src/Test/TestLab3.java
+++ src/Test/TestLab3.java
@@ -1,14 +1,27 @@
 package Test;
 
+import Lab3.NumWordPrinter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestLab3 {
 
-    @test
+    @Test
     public void even_1(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("Even", printer.printWord(2));
+    }
 
+    @Test
+    public void even_2(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("Even", printer.printWord(6));
+    }
 
+    @Test
+    public void even_3(){
+        NumWordPrinter printer = new NumWordPrinter();
+        assertEquals("Even", printer.printWord(8));
     }
 
 
