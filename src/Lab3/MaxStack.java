61bb4776cbb73c70db1ab8c7db8e931009ffeeb2
Dr-Lepic <mbmahbub007@gmail.com>
2024-09-25 20:45:39 +0600

    Created Class for Task 2 and renamed. added parameters and constructor

---
+++ src/Lab3/MaxStack.java
@@ -0,0 +1,13 @@
+package Lab3;
+
+import java.util.Stack;
+
+public class MaxStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> maxStack;
+    public MaxStack() {
+        stack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+
+}
